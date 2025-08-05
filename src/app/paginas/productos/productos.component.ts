0e2859601b3ebbf88421df83e4a671243c64aa78
Thiiagomnz <munozw512@gmail.com>
2025-08-05 10:39:05 -0300

    cinco del ocho

--- src/app/paginas/productos/productos.component.ts
+++ src/app/paginas/productos/productos.component.ts
@@ -125,4 +125,10 @@ export class ProductosComponent {
       (this.maxPrecio === null || p.precio<=this.maxPrecio)
       )
     }
+
+    mostrarSubmenu: boolean = false;
+
+filtrarPorMarca(marca: string): void {
+  this.selectedBrand = marca;
+}
 }
